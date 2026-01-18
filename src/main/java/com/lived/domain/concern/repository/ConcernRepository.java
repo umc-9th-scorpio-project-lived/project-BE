1c078954a4694ec9a93b4b7bd18358c31c0effb4
NAHO <ohmap@naver.com>
2026-01-18 17:41:48 +0900

    feat: 고민 관련 repository 정의

--- src/main/java/com/lived/domain/concern/repository/ConcernRepository.java
+++ src/main/java/com/lived/domain/concern/repository/ConcernRepository.java
@@ -1,4 +1,10 @@
 package com.lived.domain.concern.repository;
 
-public class ConcernRepository {
+import com.lived.domain.concern.entity.Concern;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ConcernRepository extends JpaRepository<Concern, Long> {
+    Optional<Concern> findByName(String name);
 }
