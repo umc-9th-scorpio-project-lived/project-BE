0a10eac3917a9f73a24752d024e5e1c952de2c26
NAHO <ohmap@naver.com>
2026-01-21 22:52:11 +0900

    fix: 오타 수정

--- src/main/java/com/lived/domain/routine/dto/RoutineBatchAddRequestDTO.java
+++ src/main/java/com/lived/domain/routine/dto/RoutineBatchAddRequestDTO.java
@@ -7,5 +7,5 @@ import java.util.List;
 @Schema(description = "추천 루틴 일관 추가 요청 DTO")
 public record RoutineBatchAddRequestDTO(
         @Schema(description = "선택된 추천 루틴 ID 리스트" , example = "[1,5,12]")
-        List<Long> routindIds
+        List<Long> routinIds
 ) {}
