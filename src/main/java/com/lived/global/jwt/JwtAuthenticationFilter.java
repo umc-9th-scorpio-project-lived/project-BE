5e693bcccca7ebc8d04386f6ba7f94c84a81375d
KimSungJun <jt2302@naver.com>
2026-01-25 02:42:18 +0900

    feat: http에서도 테스트 가능하도록 변경

--- src/main/java/com/lived/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/lived/global/jwt/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -13,6 +14,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
