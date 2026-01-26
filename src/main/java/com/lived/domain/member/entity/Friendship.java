c9b7fbaa406447820c10a99c4b21ff310089d9a2
parksooyong03 <parkwaterdragon@tukorea.ac.kr>
2026-01-26 17:29:04 +0900

    친구 엔티티 생성

--- src/main/java/com/lived/domain/member/entity/Friendship.java
+++ src/main/java/com/lived/domain/member/entity/Friendship.java
@@ -1,4 +1,35 @@
 package com.lived.domain.member.entity;
 
-public class Friendship {
+import com.lived.domain.member.enums.FriendshipStatus;
+import com.lived.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "friendship")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Friendship extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requester_id", nullable = false)
+    private Member requester; // 친구 신청을 보낸 사람
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private Member receiver; // 친구 신청을 받은 사람
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private FriendshipStatus status; // PENDING, ACCEPTED, REJECTED
+
+    @Builder.Default
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false; // 삭제 여부
 }
