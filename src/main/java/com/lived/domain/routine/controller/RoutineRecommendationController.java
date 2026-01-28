7f02eef81aba7ce09ce2f7fe7ee61b74e50b08c6
NAHO <ohmap@naver.com>
2026-01-28 19:39:45 +0900

    feat: 루틴 추천, 카테고리별 리스트 조회 API

--- src/main/java/com/lived/domain/routine/controller/RoutineRecommendationController.java
+++ src/main/java/com/lived/domain/routine/controller/RoutineRecommendationController.java
@@ -1,5 +1,6 @@
 package com.lived.domain.routine.controller;
 
+import com.lived.domain.routine.dto.RoutineRecommendResponseDTO;
 import com.lived.domain.routine.dto.RoutineResponseDTO;
 import com.lived.domain.routine.service.RoutineRecommendationService;
 import com.lived.global.apiPayload.ApiResponse;
@@ -27,13 +28,23 @@ public class RoutineRecommendationController {
             summary = "[온보딩] 고민 기반 루틴 추천 API",
             description = "사용자가 선택한 고민 ID 리스트를 전달하면 연관된 추천 루틴 목록을 반환합니다."
     )
-    @GetMapping
+    @GetMapping("/concerns")
     public ApiResponse<List<RoutineResponseDTO>> getRecommendedRoutines(
             @Parameter(description = "선택된 고민 ID 리스트", example = "1,2,3")
             @RequestParam(name = "concernIds") List<Long> concernIds
     ) {
         List<RoutineResponseDTO> response = routineRecommendationService.getRecommendedRoutines(concernIds);
 
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, response);
+    }
+
+    @Operation(
+            summary = "카테고리별 전체 추천 루틴 조회 API",
+            description = "카테고리별로 그룹화된 모든 추천 루틴 목록을 반환합니다."
+    )
+    @GetMapping("/categories")
+    public ApiResponse<RoutineRecommendResponseDTO> getRecommendedRoutinesByCategory() {
+        RoutineRecommendResponseDTO response = routineRecommendationService.getRecommendedRoutines();
         return ApiResponse.onSuccess(GeneralSuccessCode.OK, response);
     }
 }
