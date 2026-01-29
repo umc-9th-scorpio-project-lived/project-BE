f62714ec4ff157b1a1a4d31a365a57aefbbaeed7
KimSungJun <jt2302@naver.com>
2026-01-29 15:36:02 +0900

    feat: RoutineVisibilityTarget entity 추가 #72

---
+++ src/main/java/com/lived/domain/member/entity/RoutineVisibilityTarget.java
@@ -0,0 +1,25 @@
+package com.lived.domain.member.entity;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import com.lived.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class RoutineVisibilityTarget extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "target_member_id", nullable = false)
+    private Long targetMemberId;
+}
