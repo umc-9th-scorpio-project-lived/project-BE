e752dbdbbd838a0114a1b8c095d420fff3486a46
NAHO <ohmap@naver.com>
2026-02-01 21:36:05 +0900

    fix: 회원가입 시 루틴 반복 타입 누락 수정 및 엔티티 제약 조건 추가

--- src/main/java/com/lived/domain/routine/entity/mapping/MemberRoutine.java
+++ src/main/java/com/lived/domain/routine/entity/mapping/MemberRoutine.java
@@ -54,7 +54,7 @@ public class MemberRoutine extends BaseEntity {
     private LocalTime alarmTime;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "repeat_type", length = 20)
+    @Column(name = "repeat_type", nullable = false, length = 20)
     private RepeatType repeatType;
 
     @Column(name = "repeat_value", length = 100)
