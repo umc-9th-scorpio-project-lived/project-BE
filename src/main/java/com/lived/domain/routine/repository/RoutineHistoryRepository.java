ee3ff720f57f9d8cc53bdf7a5c44f63aeb8ea500
NAHO <ohmap@naver.com>
2026-02-12 16:59:40 +0900

    refactor: 미사용 쿼리 삭제

--- src/main/java/com/lived/domain/routine/repository/RoutineHistoryRepository.java
+++ src/main/java/com/lived/domain/routine/repository/RoutineHistoryRepository.java
@@ -16,10 +16,6 @@ public interface RoutineHistoryRepository extends JpaRepository<RoutineHistory, 
     // 특정 날짜의 수행 기록 조회
     Optional<RoutineHistory> findByMemberRoutineIdAndCheckDate(Long memberRoutineId, LocalDate checkDate);
 
-    // 특정 기간 내 완료된 기록 수
-    long countByMemberRoutineIdAndCheckDateBetweenAndIsDoneTrue(
-            Long memberRoutineId, LocalDate start, LocalDate end);
-
     // 특정 루틴 리스트와 날짜에 해당하는 모든 기록을 한 번에 조회
     List<RoutineHistory> findAllByMemberRoutineIdInAndCheckDate(List<Long> memberRoutineIds, LocalDate checkDate);
 
